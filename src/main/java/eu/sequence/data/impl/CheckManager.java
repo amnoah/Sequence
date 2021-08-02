498996d31e18ff490e1d83df69dd9c173ffbd70e
ExDiggers <69221879+ExDiggers@users.noreply.github.com>
2021-08-02 16:07:13 -0400

    Commit to create my branch again

--- src/main/java/eu/sequence/data/impl/CheckManager.java
+++ src/main/java/eu/sequence/data/impl/CheckManager.java
@@ -1,9 +1,6 @@
 package eu.sequence.data.impl;
 
-import com.google.common.collect.ClassToInstanceMap;
-import com.google.common.collect.ImmutableClassToInstanceMap;
 import eu.sequence.check.Check;
-import eu.sequence.check.impl.Example;
 import eu.sequence.check.impl.movement.flight.FlightStable;
 import eu.sequence.check.impl.movement.motion.MotionGravity;
 import eu.sequence.check.impl.movement.motion.MotionJump;
@@ -13,14 +10,18 @@ import eu.sequence.check.impl.movement.speed.SpeedHorizontal;
 import eu.sequence.data.PlayerData;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 public class CheckManager {
     private final List<Check> checks;
 
     public CheckManager(final PlayerData playerData) {
-       checks = Arrays.asList(new FlightStable(playerData),new MotionJump(playerData),new MotionGravity(playerData), new SpeedHorizontal(playerData));
+       checks = Arrays.asList(
+               new FlightStable(playerData),
+               new MotionJump(playerData),
+               new MotionGravity(playerData),
+               new SpeedHorizontal(playerData)
+       );
     }
 
     public List<Check> getChecks()
