172f2070f5b6764be70a41bc4a1b29cadbdfe93e
ExDiggers <69221879+ExDiggers@users.noreply.github.com>
2021-08-03 11:47:30 -0400

    fixing stuff

--- src/main/java/eu/sequence/check/impl/combat/aim/AimSigma.java
+++ src/main/java/eu/sequence/check/impl/combat/aim/AimSigma.java
@@ -4,8 +4,7 @@ import eu.sequence.check.Check;
 import eu.sequence.check.CheckInfo;
 import eu.sequence.data.PlayerData;
 import eu.sequence.data.processors.RotationProcessor;
-import eu.sequence.event.PacketEvent;
-import eu.sequence.event.PacketReceiveEvent;
+import eu.sequence.packet.Packet;
 
 @CheckInfo(name = "Aim", subName = "Sigma")
 public class AimSigma extends Check {
@@ -17,30 +16,23 @@ public class AimSigma extends Check {
     }
 
     @Override
-    public void handle(PacketEvent event) {
-        if (event instanceof PacketReceiveEvent) {
-            if (event.getPacket().isRotation()) {
+    public void handle(Packet packet) {
+        if (packet.isRotation()) {
+            final RotationProcessor rotationProcessor = playerData.getRotationProcessor();
 
-                final RotationProcessor rotationProcessor = playerData.getRotationProcessor();
+            final double deltaPitch = rotationProcessor.getDeltaPitch();
+            final double deltaYaw = rotationProcessor.getDeltaYaw();
 
-                final double deltaPitch = rotationProcessor.getDeltaPitch();
-                final double deltaYaw = rotationProcessor.getDeltaYaw();
+            final double lastDeltaYaw = this.lastDeltaYaw;
+            this.lastDeltaYaw = deltaYaw;
 
-                final double lastDeltaYaw = this.lastDeltaYaw;
-                this.lastDeltaYaw = deltaYaw;
+            final double yawAcceleration = Math.abs(deltaYaw - lastDeltaYaw);
 
-                final double yawAcceleration = Math.abs(deltaYaw - lastDeltaYaw);
-
-                if ((now() - playerData.getCombatProcessor().getLastAttackTime() < 100L)) {
-                    if (deltaPitch == 0.0D && yawAcceleration > 29.5D) {
-                        if (++this.vl > 8) {
-                            flag();
-                        }
-                    } else this.vl -= this.vl > 0 ? 0.25 : 0;
+            if (deltaPitch == 0.0D && yawAcceleration > 29.5D) {
+                if (++this.vl > 8) {
+                    flag();
                 }
-
-            }
+            } else this.vl -= this.vl > 0 ? 0.25 : 0;
         }
-
     }
 }
--- src/main/java/eu/sequence/data/impl/CheckManager.java
+++ src/main/java/eu/sequence/data/impl/CheckManager.java
@@ -1,19 +1,15 @@
 package eu.sequence.data.impl;
 
-import com.google.common.collect.ClassToInstanceMap;
-import com.google.common.collect.ImmutableClassToInstanceMap;
 import eu.sequence.check.Check;
+import eu.sequence.check.impl.combat.aim.AimSigma;
 import eu.sequence.check.impl.movement.flight.FlightAirJump;
 import eu.sequence.check.impl.movement.flight.FlightStable;
 import eu.sequence.check.impl.movement.motion.MotionGravity;
 import eu.sequence.check.impl.movement.motion.MotionJump;
-
 import eu.sequence.check.impl.movement.speed.SpeedHorizontal;
-
 import eu.sequence.data.PlayerData;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 public class CheckManager {
@@ -25,7 +21,8 @@ public class CheckManager {
                new FlightAirJump(playerData),
                new MotionJump(playerData),
                new MotionGravity(playerData),
-               new SpeedHorizontal(playerData));
+               new SpeedHorizontal(playerData),
+               new AimSigma(playerData));
     }
 
     public List<Check> getChecks()
